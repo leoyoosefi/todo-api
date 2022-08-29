b3e7326d6755d250e0ed3cb9b86cb524c3375ab5
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-30 00:26:24 +0200

    Role Dto+Exception+modelMapper+service+config

---
+++ src/main/java/se/lexicon/todoapi/model/dto/RoleDto.java
@@ -0,0 +1,14 @@
+package se.lexicon.todoapi.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RoleDto {
+
+    private Integer id;
+    private String name;
+}
